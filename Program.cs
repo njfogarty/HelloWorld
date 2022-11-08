1c9132484b6cc6e43f06afce9d7eb8cf3b85842d
Noah Fogarty <nojosoft@gmail.com>
2022-11-07 21:43:01 -0500

    Initial program state
    Brings initial project files over to GitHub.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿#region program_information
+string ver;
+ver = "1.0.0";
+Console.WriteLine("Program Version: {0}", ver); // Prints out program version information.
+#endregion
+
+#region console_output
+Console.WriteLine("Hello, World!"); // Prints out a string to console.
+Console.ReadLine(); // Pauses program.
+#endregion
